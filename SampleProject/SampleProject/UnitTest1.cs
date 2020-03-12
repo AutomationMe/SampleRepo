f5c9a8829021a275c3a88c0fb7f9b63b3fcc7088
autocurry <sandeepsekhar555@gmail.com>
2020-03-12 12:31:07 +0530

    This is the first commit command

---
+++ SampleProject/SampleProject/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleProject
+{
+    [TestClass]
+    public class PrintData
+    {
+        [TestMethod]
+        public void printFirst()
+        {
+            Console.WriteLine("This is a print command");
+        }
+    }
+}
